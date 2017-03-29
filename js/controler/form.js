4d421e6b59bbbbda3d8a202282f3df4e0584db0f
Allan <balaju01@gmail.com>
2017-03-29 03:03:03 -0600

    seleccionar boton y obtener opcion de seleccionar unicamento 2 mas

--- js/controler/form.js
+++ js/controler/form.js
@@ -3,6 +3,10 @@ app.controller("formulario",['$scope','$http',function($scope,$http){
         
         $scope.user = {};
         $scope.lugar = [];
+        $scope.asiento = [];
+        for (var i = 0; i < 31; i++) {
+          $scope.asiento[i] = false;
+        }
  
         $scope.update = function() {
           $scope.user.accion = "alta";
@@ -24,8 +28,32 @@ app.controller("formulario",['$scope','$http',function($scope,$http){
         $scope.reset();
 
         $scope.seleccionaBoton = function(lug) {
+          a = 0;
+          b = 0;
+          if ($scope.lugar.length == 0) {
+            if (lug == 1 || lug == 7 || lug == 16 || lug == 23) {
+              a = lug;
+              b = lug + 3;
+            }
+            else if (lug == 6 || lug == 15 || lug == 22 || lug == 29) {
+              a = lug - 2;
+              b = lug + 1;
+            }
+            else {
+              a = lug - 1;
+              b = lug + 2;
+            }
+            for (var j = 0; j < 31; j++) {
+              $scope.asiento[j] = true;
+            }
+            for (var i = a; i < b; i++) {
+              $scope.asiento[i] = false;
+            }
+          }
           $scope.lugar.push(lug);
+          $scope.asiento[lug] = true;
           $scope.user.lugar = $scope.lugar.join();
+          console.log($scope.asiento);
         };
 
 }]);
