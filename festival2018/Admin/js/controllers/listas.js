fb42d193a3f3138580baee202b713c35929c3878
balaju01 <balaju01@gmail.com>
2018-07-23 22:21:02 -0500

    Categorias de Productos, despliegue de listas por status y por area, botones de cambio de estado abilitado

--- festival2018/Admin/js/controllers/listas.js
+++ festival2018/Admin/js/controllers/listas.js
@@ -3,15 +3,16 @@ app.controller("listados",['$scope','$rootScope','$http','auth',function($scope,
     console.log("lista general ");
     $scope.status = 0;
     
-    $scope.reservado = function()
+    $scope.reservado = function(seccion,estos)
     {
-    	$scope.status = 0;
+    	$scope.status = estos;
+    	
     	var req = {
             method : "POST" ,
             url :  "http://35.231.193.133/busqueda/participantes/estado" , 
             data: {
-            	secction:"",
-            	estado:0
+            	secction:seccion,
+            	estado:estos
             }
         };
         console.log(req);
@@ -19,23 +20,24 @@ app.controller("listados",['$scope','$rootScope','$http','auth',function($scope,
           console.log(response);
           $scope.data = response;
           angular.forEach($scope.data,function(expositor) {
-
-          	
-	        if ((new Date(expositor.fecha).getMonth())==(new Date().getMonth())) 
-	        {
-	        	if ((new Date(expositor.fecha).getDate())==(new Date().getDate())) 
-	        	{
-	        		expositor.incrip = "hoy";
-	        	}
-	        	else
+          	if ($scope.status == 0)
+          	{
+		        if ((new Date(expositor.fecha).getMonth())==(new Date().getMonth())) 
+		        {
+		        	if ((new Date(expositor.fecha).getDate())==(new Date().getDate())) 
+		        	{
+		        		expositor.incrip = "hoy";
+		        	}
+		        	else
+			        {
+			        	expositor.incrip = "no";
+			        }
+		        }
+		        else
 		        {
 		        	expositor.incrip = "no";
 		        }
-	        }
-	        else
-	        {
-	        	expositor.incrip = "no";
-	        }
+		    }
           });
           console.log($scope.data);
 
@@ -49,79 +51,36 @@ app.controller("listados",['$scope','$rootScope','$http','auth',function($scope,
 
     var init = function()
     {
-    	$scope.reservado();
+    	$scope.reservado("",0);
     };
     init();
 
-    $scope.confirmados = function()
-    {
-    	$scope.status = 1;
-    	var req = {
-            method : "POST" ,
-            url :  "http://35.231.193.133/busqueda/participantes/estado" , 
-            data: {
-            	secction:"",
-            	estado:1
-            }
-        };
-        console.log(req);
-        $http(req).success(function (response) {//'response' es el objeto que devuelve el servicio web
-          console.log(response);
-          $scope.data = response;
-
-        }).error(function (response){
-          console.log(response);
-          alert("Ha fallado la petici\u00F3n. Estado HTTP:"+status);
-        });
-    };
-
-    $scope.pagados = function()
-    {
-    	$scope.status = 2;
-    	var req = {
-            method : "POST" ,
-            url :  "http://35.231.193.133/busqueda/participantes/estado" , 
-            data: {
-            	secction:"",
-            	estado:2
-            }
-        };
-        console.log(req);
-        $http(req).success(function (response) {//'response' es el objeto que devuelve el servicio web
-          console.log(response);
-          $scope.data = response;
-
-        }).error(function (response){
-          console.log(response);
-          alert("Ha fallado la petici\u00F3n. Estado HTTP:"+status);
-        });
-    };
-
-    $scope.rechazados = function()
-    {
-    	$scope.status = 3;
-    	var req = {
-            method : "POST" ,
-            url :  "http://35.231.193.133/busqueda/participantes/estado" , 
-            data: {
-            	secction:"",
-            	estado:3
-            }
-        };
-        console.log(req);
-        $http(req).success(function (response) {//'response' es el objeto que devuelve el servicio web
-          console.log(response);
-          $scope.data = response;
-
-        }).error(function (response){
-          console.log(response);
-          alert("Ha fallado la petici\u00F3n. Estado HTTP:"+status);
-        });
-    }
+    
     
     $scope.logout = function()
     {
         auth.logout();
+    };
+
+    $scope.cambioEstado = function(est,ids)
+    {
+    	var req = {
+            method : "POST" ,
+            url :  "http://35.231.193.133/actualizar/participante" , 
+            data: {
+            	estado:est,
+            	id:ids
+            }
+        };
+        console.log(req);
+        $http(req).success(function (response) {//'response' es el objeto que devuelve el servicio web
+          console.log(response);
+          alert("El Expositor Cambio de Status exitosamente");
+
+        }).error(function (response){
+          console.log(response);
+          alert("Ha fallado la petici\u00F3n. Estado HTTP:"+status);
+        });
     };
     
 }]);
