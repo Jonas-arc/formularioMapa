f0413a8a6cb547d0abf12922a05cf7f04497a24e
balaju01 <balaju01@gmail.com>
2019-06-05 01:37:43 -0500

    Agregar zonas al sistema

--- festival2019/FRONT/app.js
+++ festival2019/FRONT/app.js
@@ -3,9 +3,13 @@ app.config(function($routeProvider) {
    $routeProvider
    .when('/' ,{templateUrl: './views/expo2019.html', controller: 'cat'})
    .when('/GastronomoA' ,{templateUrl: './views/GastronomoA.html', controller: 'formulario'})
+   .when('/ZonaA' ,{templateUrl: './views/ZonaA.html', controller: ''})
    .when('/GastronomoB' ,{templateUrl: './views/GastronomoB.html', controller: 'formulario'})
+   .when('/ZonaB' ,{templateUrl: './views/ZonaB.html', controller: ''})
    .when('/GastronomoC' ,{templateUrl: './views/GastronomoC.html', controller: 'formulario'})
+   .when('/ZonaC' ,{templateUrl: './views/ZonaC.html', controller: ''})
    .when('/GastronomoD' ,{templateUrl: './views/GastronomoD.html', controller: 'formulario'})
+   .when('/ZonaD' ,{templateUrl: './views/ZonaD.html', controller: ''})
    .when('/GastronomoE' ,{templateUrl: './views/GastronomoE.html', controller: 'formulario'})
    .when('/Artesano' ,{templateUrl: './views/Artesano.html', controller: 'formulario'})
    .when('/Fin' ,{templateUrl: './views/fin.html', controller: ''})
